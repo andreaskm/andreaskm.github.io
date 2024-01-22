e8c0a013748198c554d5fad91977382f0301fd71
Andreas Krøger Madsen <Andreas.krogerm@gmail.com>
2024-01-22 12:07:09 +0100

    Styles, ts, grid layout etc

---
+++ src/components/application/mapApplication.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./application.css";
+
+export function MapApplication(){
+    return <>
+        <header><h1>Map Application</h1></header>
+        <nav>Actions</nav>
+        <main>Map here</main>
+    </>
+}
