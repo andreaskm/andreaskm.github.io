74bd9c1ba3c92847a814c9920c35b9d5cf081ac6
Andreas Krøger Madsen <Andreas.krogerm@gmail.com>
2024-01-24 18:45:19 +0100

    First

---
+++ src/application/application.tsx
@@ -0,0 +1,32 @@
+import { Map, View } from "ol";
+import { Layer } from "ol/layer";
+import { OSM } from "ol/source";
+import TileLayer from "ol/layer/Tile";
+import { useGeographic } from "ol/proj";
+import { MutableRefObject, useEffect, useRef } from "react";
+import React from "react";
+import "ol/ol.css";
+import "./application.css";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [10, 60], zoom: 9 }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
+
+export function Application() {
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+
+  return (
+    <>
+      <header>header</header>
+      <nav>nav</nav>
+      <div ref={mapRef}></div>
+    </>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Application } from "./application/application";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<Application />);
