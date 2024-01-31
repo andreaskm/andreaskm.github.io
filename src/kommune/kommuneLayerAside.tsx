8e2f2275d246847f64c2ccda0d09b9bce52cf073
Andreas Krøger Madsen <andreas.krogerm@gmail.com>
2024-01-31 15:20:16 +0100

    Showing kommunenavn

--- src/kommune/kommuneLayerAside.tsx
+++ src/kommune/kommuneLayerAside.tsx
@@ -1,25 +1,62 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MapContext } from "../map/mapcontext";
 import React from "react";
 import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import { Feature } from "ol";
 
-export function KommuneAside() {
+type KommuneVectorLayer = VectorLayer<VectorSource<KommuneFeature>>;
+
+interface KommuneProperties {
+  kommunenummer: string;
+  navn: Stedsnavn[];
+}
+
+interface Stedsnavn {
+  sprak: string;
+  navn: string;
+}
+
+type KommuneFeature = {
+  getProperties(): KommuneProperties;
+} & Feature;
+
+function getStedsnavn(navn: Stedsnavn[]) {
+  return navn.find((navn) => navn.sprak === "nor")?.navn;
+}
+
+function useKommuneFeatures() {
   const { layers } = useContext(MapContext);
 
   const kommuneLayer = layers.find(
     (layer) => layer.getClassName() === "kommuner",
-  );
+  ) as KommuneVectorLayer;
 
-  const features = (kommuneLayer?.getSource() as VectorSource)?.getFeatures();
+  const [features, setFeatures] = useState<KommuneFeature[]>();
+
+  function handleSourceChange() {
+    setFeatures(kommuneLayer?.getSource()?.getFeatures());
+  }
+
+  useEffect(() => {
+    kommuneLayer?.getSource()?.on("change", handleSourceChange);
+    return () => kommuneLayer?.getSource()?.un("change", handleSourceChange);
+  }, [kommuneLayer]);
+
+  return { kommuneLayer, features };
+}
+
+export function KommuneAside() {
+  const { features } = useKommuneFeatures();
 
   return (
     <>
-      <aside className={kommuneLayer ? "visible" : "hidden"}>
+      <aside className={features?.length ? "visible" : "hidden"}>
         <div>
           <h2>Kommuner</h2>
           <ul>
             {features?.map((kommune) => (
-              <li>{kommune.getProperties().kommunenummer}</li>
+              <li>{getStedsnavn(kommune.getProperties().navn)}</li>
             ))}
           </ul>
         </div>
