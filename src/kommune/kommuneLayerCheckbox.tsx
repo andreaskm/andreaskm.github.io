4d0b38e3b57bd82be56e47267d46e83b282b2367
Andreas Krøger Madsen <Andreas.krogerm@gmail.com>
2024-01-31 21:13:57 +0100

    Clicking on kommune now shows name in an overlay at the position it was clicked.

--- src/kommune/kommuneLayerCheckbox.tsx
+++ src/kommune/kommuneLayerCheckbox.tsx
@@ -1,10 +1,18 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  MutableRefObject,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { MapContext } from "../map/mapcontext";
-import { Feature, MapBrowserEvent } from "ol";
+import { Feature, MapBrowserEvent, Overlay } from "ol";
 import { Polygon } from "ol/geom";
+import "../application/application.css";
 
 type KommuneProperties = {
   kommunenummer: string;
@@ -31,6 +39,22 @@ const kommunelayer = new VectorLayer({
 export function KommuneLayerCheckbox() {
   const [checked, setChecked] = useState(false);
 
+  const overlay = useMemo(() => new Overlay({}), []);
+  const overlayRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    overlay.setElement(overlayRef.current);
+    map.addOverlay(overlay);
+
+    return () => {
+      map.removeOverlay(overlay);
+    };
+  }, []);
+
+  const [selectedKommune, setSelectedKommune] = useState<
+    kommuneFeature | undefined
+  >();
+
   function handleClick(e: MapBrowserEvent<MouseEvent>) {
     const clickedKommune = kommuneSource.getFeaturesAtCoordinate(
       e.coordinate,
@@ -38,8 +62,11 @@ export function KommuneLayerCheckbox() {
 
     if (clickedKommune.length === 1) {
       const properties = clickedKommune[0].getProperties() as KommuneProperties;
-      alert(properties.navn.find((navn) => navn.sprak === "nor")!.navn);
-      console.log(properties.navn.find((navn) => navn.sprak === "nor")!.navn);
+      setSelectedKommune(clickedKommune[0]);
+      overlay.setPosition(e.coordinate);
+    } else {
+      setSelectedKommune(undefined);
+      overlay.setPosition(undefined);
     }
   }
 
@@ -53,19 +80,33 @@ export function KommuneLayerCheckbox() {
     //Kjører før useEffect er ferdig for å fjerne kommune layer
     return () => {
       map.un("click", handleClick);
+      setSelectedKommune(undefined);
       setLayers((old) => old.filter((layer) => layer !== kommunelayer));
     };
   }, [checked]);
 
   return (
-    <label>
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
-      />
-      {checked ? " HIDE " : " SHOW "}
-      KOMMUNER
-    </label>
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        {checked ? " HIDE " : " SHOW "}
+        KOMMUNER
+      </label>
+      <div ref={overlayRef} className={"kommune-overlay"}>
+        {selectedKommune && (
+          <>
+            {
+              (selectedKommune.getProperties() as KommuneProperties).navn.find(
+                (navn) => navn.sprak === "nor",
+              )!.navn
+            }
+          </>
+        )}
+      </div>
+    </>
   );
 }
