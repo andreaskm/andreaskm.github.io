f8b0798c02f683bfe9eed8d36b9801de13145cd5
Andreas Krøger Madsen <andreas.krogerm@gmail.com>
2024-02-07 15:28:54 +0100

    Kraftverkstyle change on hover

--- src/kraftverk/kraftverkLayerCheckbox.tsx
+++ src/kraftverk/kraftverkLayerCheckbox.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { useLayer } from "../map/useLayer";
 import { FeatureLike } from "ol/Feature";
-import { Feature } from "ol";
+import { Feature, MapBrowserEvent } from "ol";
 import { Point } from "ol/geom";
 import { Circle, Fill, Stroke, Style } from "ol/style";
+import { MapContext } from "../map/mapcontext";
 
 const kraftverkLayer = new VectorLayer({
   source: new VectorSource({
@@ -41,9 +42,54 @@ function kraftverkStyle(f: FeatureLike) {
   });
 }
 
+function activeKraftverkStyle(f: FeatureLike) {
+  const feature = f as KraftverkFeature;
+  const kraftverk = feature.getProperties();
+
+  return new Style({
+    image: new Circle({
+      stroke: new Stroke({ color: "white", width: 1.5 }),
+      fill: new Fill({ color: "green" }),
+      radius: 8,
+    }),
+  });
+}
+
 export function KraftverkLayerCheckbox() {
+  const { map } = useContext(MapContext);
   const [checked, setChecked] = useState(true);
+
+  const [activeFeature, setActiveFeature] = useState<KraftverkFeature>();
+
+  function handlePointerMove(e: MapBrowserEvent<MouseEvent>) {
+    const features: FeatureLike[] = [];
+
+    map.forEachFeatureAtPixel(e.pixel, (f) => features.push(f), {
+      hitTolerance: 5,
+      layerFilter: (l) => l === kraftverkLayer,
+    });
+
+    if (features.length === 1) {
+      setActiveFeature(features[0] as KraftverkFeature);
+    } else {
+      setActiveFeature(undefined);
+    }
+  }
+
+  useEffect(() => {
+    activeFeature?.setStyle(activeKraftverkStyle);
+    return () => activeFeature?.setStyle(undefined);
+  }, [activeFeature]);
+
   useLayer(kraftverkLayer, checked);
+
+  useEffect(() => {
+    if (checked) {
+      map.on("pointermove", handlePointerMove);
+    }
+    return () => map.un("pointermove", handlePointerMove);
+  }, []);
+
   return (
     <>
       <label>
