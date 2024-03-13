b11e7378b50c6ee8a58f8c3251b60fd78fe4d1fe
Andreas Krøger Madsen <andreas.krogerm@gmail.com>
2024-03-13 14:21:48 +0100

    server.js

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,24 +1,29 @@
+import * as React from "react";
+import { Map, View } from "ol";
 import { useGeographic } from "ol/proj";
-import { View, Map } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { MutableRefObject, useEffect, useRef } from "react";
-import React from "react";
-import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
 
 useGeographic();
-
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
-  view: new View({ center: [11, 60], zoom: 10 }),
+  view: new View({ center: [10, 60], zoom: 8 }),
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/api/kommuner",
+        format: new GeoJSON(),
+      }),
+    }),
+  ],
 });
 
 export function Application() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-
-  useEffect(() => {
-    map.setTarget(mapRef.current);
-  }, []);
-
+  useEffect(() => map.setTarget(mapRef.current), []);
   return <div ref={mapRef}></div>;
 }
