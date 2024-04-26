8d3ef6f438798b732fa0fffff06bae0ce37e25af
Andreas Krøger Madsen <Andreas.krogerm@gmail.com>
2024-04-26 13:56:03 +0200

    Freehandmode and overlay for Draw

--- src/components/hooks/useDrawLineString.tsx
+++ src/components/hooks/useDrawLineString.tsx
@@ -5,11 +5,13 @@ import { drawingSource } from "../application";
 import { getLength } from "ol/sphere";
 import { useGeographic } from "ol/proj";
 import { LineString } from "ol/geom";
+import { Overlay } from "ol";
+
 useGeographic();
 const formatLength = function (line: LineString) {
   const length = getLength(line) * 100000;
   let output;
-  if (length > 100) {
+  if (length > 1000) {
     output = Math.round((length / 1000) * 100) / 100 + " " + "km";
   } else {
     output = Math.round(length * 100) / 100 + " " + "m";
@@ -17,25 +19,59 @@ const formatLength = function (line: LineString) {
   return output;
 };
 
-function UseDrawLineString(checked: boolean) {
+function UseDrawLineString(checked: boolean, freehand?: boolean) {
   const [lengthOfString, setLengthOfString] = useState<string>();
-  const draw = new Draw({ type: "LineString", source: drawingSource });
+  //const [previousMeasures, setPreviousMeasures] = useState<string[]>([]);
+  let draw: Draw;
+
   let sketch;
+
+  const overlayElement = document.createElement("div");
+  overlayElement.className = "hover-overlay";
+  const overlay = new Overlay({
+    element: overlayElement,
+    positioning: "bottom-center",
+    offset: [0, -10],
+  });
 
   useEffect(() => {
     function handleDrawLineString() {
+      if (!freehand) {
+        draw = new Draw({
+          type: "LineString",
+          source: drawingSource,
+        });
+      } else {
+        draw = new Draw({
+          type: "LineString",
+          source: drawingSource,
+          freehand: true,
+        });
+      }
+
+      let tooltipCoord;
       map.addInteraction(draw);
       let listener;
+      console.log(lengthOfString);
       draw.on("drawstart", (e) => {
         sketch = e.feature;
         listener = sketch.getGeometry()!.on("change", (e) => {
           const geometry = e.target;
           let output;
           output = formatLength(geometry);
+          tooltipCoord = geometry.getLastCoordinate();
           setLengthOfString(output);
+          map.addOverlay(overlay);
+          overlayElement.innerHTML = output;
+          overlay.setPosition(tooltipCoord);
         });
       });
+
+      draw.on("drawend", () => {
+        map.removeOverlay(overlay);
+      });
     }
+
     if (checked) {
       console.log(lengthOfString);
       handleDrawLineString();
@@ -43,8 +79,9 @@ function UseDrawLineString(checked: boolean) {
 
     return () => {
       map.removeInteraction(draw);
+      map.removeOverlay(overlay);
     };
-  }, [checked]);
+  }, [checked, freehand]);
 
   return lengthOfString;
 }
--- src/components/sidebar/checkboxes/LineStringCheckbox.tsx
+++ src/components/sidebar/checkboxes/LineStringCheckbox.tsx
@@ -3,11 +3,12 @@ import useDrawLineString from "../../hooks/useDrawLineString";
 
 function LineStringCheckbox() {
   const [checked, setChecked] = useState(false);
+  const [freehand, setFreehand] = useState(false);
 
-  const lengthOfString = useDrawLineString(checked);
+  const lengthOfString = useDrawLineString(checked, freehand);
 
   return (
-    <div className={"checkbox"}>
+    <div className={"checkbox full-width"}>
       <label>
         <input
           type="checkbox"
@@ -15,11 +16,25 @@ function LineStringCheckbox() {
           onChange={(e) => setChecked(e.target.checked)}
         />
         {checked
-          ? "Stop drawing. " +
-            lengthOfString +
-            " (Double click to save length.)"
+          ? "Stop drawing. " + " (Double click to save length.)"
           : "Draw"}
       </label>
+      {checked ? (
+        <div className={"freehand"}>
+          <label>
+            <input
+              type="checkbox"
+              checked={freehand}
+              onChange={(e) => setFreehand(e.target.checked)}
+            />
+            {freehand ? "Turn off freehand " : "Turn on freehand"}
+          </label>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <div className={"length-style"}>{checked ? lengthOfString : ""}</div>
     </div>
   );
 }
